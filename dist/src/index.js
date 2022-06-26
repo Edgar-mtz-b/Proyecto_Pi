290421be76633ccf867cf104621a197ae9af5469
EdgarMartinezBolaños <edgar.mtz.bls@gmail.com>
2022-06-26 16:06:43 -0500

    prueba2

--- dist/src/index.js
+++ dist/src/index.js
@@ -211,8 +211,8 @@ function animate() {
     */
     // imagenSal = new ImageType(pantalla1, null, 300, 300, true);
     var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
-    var imagen2 = new ImageType(pantalla4, imgLocal4.getImage());
-    imagenSal.imageArray2DtoDataWithResizing(pantalla2, MathImg.marcaAguaArray(imagenSal, imagen2, 0.25));
+    var imagen2 = new ImageType(pantalla1, imgLocal4.getImage());
+    imagenSal.imageArray2DtoDataWithResizing(pantalla1, MathImg.marcaAguaArray(imagenSal, imagen2, 0.25));
     initParticles();
     animateParticles();
 }
--- src/index.ts
+++ src/index.ts
@@ -223,7 +223,7 @@ function animate() {
   */
  // imagenSal = new ImageType(pantalla1, null, 300, 300, true);
  var imagenSal: ImageType = new ImageType(pantalla1, imgLocal.getImage());
-  var imagen2:ImageType = new ImageType(pantalla1, imgLocal4.getImage());
+  var imagen2:ImageType = new ImageType(pantalla1, imgLocal.getImage());
  imagenSal.imageArray2DtoDataWithResizing(pantalla1, MathImg.marcaAguaArray(imagenSal, imagen2, 0.25));
   initParticles();
   animateParticles();
