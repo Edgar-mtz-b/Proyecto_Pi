c6d6c6a0458bb0e8a4dc7ed80c03399c46c41bec
EdgarMartinezBolaños <edgar.mtz.bls@gmail.com>
2022-06-26 19:33:28 -0500

    pruebas

--- dist/src/particle.js
+++ dist/src/particle.js
@@ -74,7 +74,12 @@ var ParticleText = /** @class */ (function () {
         this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         this.ctx.closePath();
         this.ctx.fill();
-        requestAnimationFrame(animate);
+        var particle1 = new ParticleText(100, 10);
+        function animate() {
+            particle1.update(MouseEvent);
+            particle1.draw();
+            requestAnimationFrame(animate);
+        }
     };
     return ParticleText;
 }());
