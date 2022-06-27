373d4fc6abd737ac120fc5e744f594a37920d5e6
EdgarMartinezBolaños <edgar.mtz.bls@gmail.com>
2022-06-26 19:37:41 -0500

    buscando color particula

--- src/particle.ts
+++ src/particle.ts
@@ -104,19 +104,11 @@ export class ParticleText {
   }
   public draw()
   {
-    this.ctx.fillStyle='red';
+    this.ctx.fillStyle = 'blue';
     this.ctx.beginPath();
-    this.ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
+    this.ctx.arc(this.x, this.y, this.size, 0, this._2PI);
     this.ctx.closePath();
     this.ctx.fill();
-  this.ctx.fillStyle='rgba(255,255,0.01)';
-    let  particle1 =new ParticleText(100,50);
-    function animate(){
-      
-      particle1.update(MouseEvent);
-      particle1.draw();
-      requestAnimationFrame(animate);
-    }
   }
   
   /*
