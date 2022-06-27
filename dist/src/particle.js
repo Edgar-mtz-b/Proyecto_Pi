ddd9f915413a115be2bc50045643c24f95252528
EdgarMartinezBolaños <edgar.mtz.bls@gmail.com>
2022-06-26 19:09:48 -0500

    cambios particulas

--- dist/src/particle.js
+++ dist/src/particle.js
@@ -41,48 +41,80 @@ var Particle = /** @class */ (function () {
 }());
 export { Particle };
 var ParticleText = /** @class */ (function () {
+    /*
+    constructor(x: number, y: number, screenCanvas?: CanvasRenderingContext2D,
+      mapImg?: number[][][]) {
+      this.ctx = screenCanvas;
+      this.x = x;// + 200;
+      this.y = y;// - 100,
+      this.size = 1;
+      this.baseX = this.x;
+      this.baseY = this.y;
+      this.density = ((Math.random() * 30) + 1);
+      this._2PI = Math.PI * 2;
+      this.mappedImage = mapImg;
+    }
+  */
     function ParticleText(x, y, screenCanvas, mapImg) {
-        this.ctx = screenCanvas;
-        this.x = x; // + 200;
-        this.y = y; // - 100,
-        this.size = 1;
-        this.baseX = this.x;
-        this.baseY = this.y;
-        this.density = ((Math.random() * 30) + 1);
-        this._2PI = Math.PI * 2;
-        this.mappedImage = mapImg;
+        this.x = x;
+        this.y = y;
+        this.size = 10;
+        this.weight = 2;
+        this.directionX = 1;
     }
     ParticleText.prototype.update = function (mouse) {
-        var dx = mouse.x - this.x;
-        var dy = mouse.y - this.y;
-        var distance = Math.sqrt(dx * dx + dy * dy);
-        var forceDirectionX = dx / distance;
-        var forceDirectionY = dy / distance;
-        var maxDistance = mouse.radius;
-        var force = (maxDistance - distance) / maxDistance;
-        var directionX = (forceDirectionX * force * this.density);
-        var directionY = (forceDirectionY * force * this.density);
-        if (distance < mouse.radius) {
-            this.x -= directionX;
-            this.y -= directionY;
-        }
-        else {
-            if (this.x !== this.baseX) {
-                var dx_1 = this.x - this.baseX;
-                this.x -= dx_1 / 5;
-            }
-            if (this.y !== this.baseY) {
-                var dy_1 = this.y - this.baseY;
-                this.y -= dy_1 / 5;
-            }
-        }
+        this.weight += 0.01;
+        this.y += this.weight;
     };
     ParticleText.prototype.draw = function () {
-        this.ctx.fillStyle = 'blue';
+        this.ctx.fillStyle = 'red';
         this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.size, 0, this._2PI);
+        this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         this.ctx.closePath();
         this.ctx.fill();
+        var particle1 = new ParticleText(100, 10);
+        function animate() {
+            particle1.update(MouseEvent);
+            particle1.draw();
+            requestAnimationFrame(animate);
+        }
+        /*
+        public update(mouse: any) {
+          let dx = mouse.x - this.x;
+          let dy = mouse.y - this.y;
+          let distance = Math.sqrt(dx*dx + dy*dy);
+          let forceDirectionX = dx / distance;
+          let forceDirectionY = dy / distance;
+          var maxDistance = mouse.radius;
+          var force = (maxDistance - distance) / maxDistance;
+      
+          let directionX = (forceDirectionX * force * this.density);
+          let directionY = (forceDirectionY * force * this.density);
+          
+          if (distance < mouse.radius) {
+            this.x -= directionX ;
+            this.y -= directionY ;
+          }
+          else {
+            if (this.x !== this.baseX ) {
+                let dx = this.x - this.baseX;
+                this.x -= dx/5;
+            } if (this.y !== this.baseY) {
+                let dy = this.y - this.baseY;
+                this.y -= dy/5;
+            }
+          }
+        }
+      */
+        /*
+          public draw() {
+            this.ctx.fillStyle = 'blue';
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.size, 0, this._2PI);
+            this.ctx.closePath();
+            this.ctx.fill();
+          }
+        */
     };
     return ParticleText;
 }());
