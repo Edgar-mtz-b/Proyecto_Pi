cc36fc75bf814712653d1bd435b1e30bec3eec50
EdgarMartinezBolaños <edgar.mtz.bls@gmail.com>
2022-06-27 01:10:33 -0500

    no guardo el pixelessa1

--- src/particle.ts
+++ src/particle.ts
@@ -48,8 +48,9 @@ export class Particle {
 
   public draw() {
     this.ctx.beginPath();
-    this.ctx.lineWidth=25;
+   
     this.ctx.fillStyle = this.mappedImage[1][this.position1][this.position2];
+    this.ctx.lineWidth=50;
     //this.ctx.fillStyle = 'white';
     this.ctx.arc(this.x, this.y, this.size, 0, this._2PI);
     this.ctx.fill();
